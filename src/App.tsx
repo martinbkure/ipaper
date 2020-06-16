4198c58971af4bf714bccf233c4334e979209030
martin k <mku@dotlegal.dk>
2020-06-16 13:17:09 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import ReactSlider from 'react-slider';
+import FontRenderer from './FontRendere';
+
+function calculateFlipBooksPrice(flipBooks: number): number {
+  let calculatedPrice = 0;
+  let i;
+  for (i = 1; i <= flipBooks; i++) {
+    if(i < 6) {
+      calculatedPrice += 5;
+    } else if (i > 5 && i < 11) {
+      calculatedPrice += 2;
+    } else {
+      calculatedPrice += 1;
+    }
+  }
+  return calculatedPrice;
+}
+
+function getIncludedSessions(flipBooks: number): number {
+  let sessions = 0;
+  let i;
+  for (i = 1; i <= flipBooks; i++) {
+    if(i < 6) {
+      sessions += 2000;
+    } else if (i > 5 && i < 11) {
+      sessions += 1000;
+    } else {
+      sessions += 250;
+    }
+  }
+  return sessions;
+}
+
+function calculateExtraSessionsPrice(sessions: number): number {
+  let calculatedPrice: number = 0;
+  let i;
+
+  // include sessions that doesn't add up to 1000
+  const sessionNotEqualToThousand: number = sessions % 1000; 
+  const counter: number = sessionNotEqualToThousand > 0 ? sessions + (1000 - sessionNotEqualToThousand) : sessions;
+
+  for (i = 1000; i <= counter; i+=1000) {
+    if(i < 11000) {
+      calculatedPrice += 1.5;
+    } else if (i > 10000 && i < 51000) {
+      calculatedPrice += 1;
+    } else {
+      calculatedPrice += 0.75;
+    }
+  }
+  return calculatedPrice;
+}
+
+function App() {
+
+  const [flipBooks, setFlipBooks] = useState<number>(1);
+  const [sessions, setSessions] = useState<number>(1000);
+  const [price, setPrice] = useState<number>(5);
+
+  useEffect(() => {
+    
+    let sessionsPrice = 0;
+    const flipBooksPrice: number = calculateFlipBooksPrice(flipBooks);
+    const includedSessions: number = getIncludedSessions(flipBooks);
+    if(includedSessions < sessions){
+      sessionsPrice = calculateExtraSessionsPrice(sessions - includedSessions);
+    }
+
+    setPrice(flipBooksPrice + sessionsPrice);
+  }, [sessions, flipBooks]);
+
+  return (
+    <div className="App">
+      <div>
+        <label>iPapers needed:</label>
+        <ReactSlider
+          step={ 1 }
+          min={ 1 }
+          className="horizontal-slider"
+          defaultValue={ flipBooks }
+          thumbClassName="example-thumb"
+          trackClassName="example-track"
+          onChange={val => setFlipBooks(val as number)}
+          renderThumb={(props, _) => <div {...props}>{flipBooks}</div>}
+        />
+      </div>
+      <div>
+        <label>Expected visitors pr month:</label>
+        <ReactSlider
+            className="horizontal-slider"
+            step={ 1000 }
+            min={ 1000 }
+            max={ 100000 }
+            defaultValue={ sessions }
+            thumbClassName="example-thumb"
+            trackClassName="example-track"
+            onChange={val => setSessions(val as number)}
+            renderThumb={(props, _) => <div {...props}>{sessions}</div>}
+        />
+      </div>
+
+      <p>Your price: € <span style={{ fontWeight: 'bold' }}>{ price}/month</span></p>
+
+
+      <FontRenderer></FontRenderer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FontRendere.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function FontRenderer() {
+
+  const [ width, setWidth ] = useState(0);
+  const [ height, setHeight ] = useState(0);
+  const [ text, setText ] = useState('');
+  const [ font, setFont ] = useState('');
+
+  const [ displayedWidth, setDisplayWidth ] = useState(0);
+  const [ displayedHeight, setDisplayHeight ] = useState(0);
+  const [ displayedText, setDisplayText ] = useState('');
+  const [ displayedFont, setDisplayFont ] = useState('');
+
+  function renderText() {
+    setDisplayWidth(width);
+    setDisplayHeight(height);
+    setDisplayText(text);
+    setDisplayFont(font);
+  }
+
+  return (
+    <div>
+      <input type="number" onChange={event => setWidth(Number(event.target.value)) } placeholder="width"></input>
+      <input type="number" onChange={event => setHeight(Number(event.target.value)) } placeholder="height"></input>
+      <input type="text" placeholder="text" onChange={event => setText(event.target.value) }></input>
+      <select onChange={event => setFont(event.target.value) }>
+        <option value="Verdana">Verdana</option>
+        <option value="Ariel">Ariel</option>
+        <option value="Times New Roman">Times New Roman</option>
+      </select>
+      <button onClick={renderText}>Render text</button>
+      <div style={{ width: displayedWidth, 
+                    display: 'flex',
+                    justifyContent:'center',
+                    alignItems: 'center',
+                    fontSize: `${ ((displayedWidth/window.innerWidth)* 100) / displayedText.length }vw`,
+                    height: displayedHeight, 
+                    fontFamily: displayedFont }}>
+        <p>{ displayedText }</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default FontRenderer;
